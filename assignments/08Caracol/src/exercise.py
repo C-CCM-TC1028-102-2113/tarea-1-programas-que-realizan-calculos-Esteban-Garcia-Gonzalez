429681e4e630f97184bfb70815c632b91afa5f0f
Esteban-Garcia-Gonzalez <89358442+Esteban-Garcia-Gonzalez@users.noreply.github.com>
2021-08-22 15:53:31 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,13 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    ##Caracol
+    pass
+ ##Caracol
 m=0
 cm=0
 print('Dame los minutos:')
 m=float(input())
 cm= m*34.2
 print('Centímentros recorridos:', cm)
-    pass
 
 if __name__ == '__main__':
     main()
