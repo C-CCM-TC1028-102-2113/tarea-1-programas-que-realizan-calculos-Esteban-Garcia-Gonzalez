e71facb1e1189eb1f5e06b6674f61a0df02ba863
Esteban-Garcia-Gonzalez <89358442+Esteban-Garcia-Gonzalez@users.noreply.github.com>
2021-08-22 17:31:56 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,6 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
     ##Caracol
+    m=0
+    cm=0
     m= float(input('Dame los minutos: '))
     cm= (m)*34.2
     print('Centímentros recorridos:',cm)
