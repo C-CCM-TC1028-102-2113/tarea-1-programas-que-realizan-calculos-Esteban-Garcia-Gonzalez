cbe0c2b8c1437f5b9415ef2ef637d851036aac42
Esteban-Garcia-Gonzalez <89358442+Esteban-Garcia-Gonzalez@users.noreply.github.com>
2021-08-22 17:44:05 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,11 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    time=round(float(input('Dame los minutos: ')),1)
+    m=round(float(input('Dame los minutos: ')),1)
 
-    vel=5.7
-    dist=float((vel*6)*time)
+    v=5.7
+    c=float((v*6)*m)
 
-    print('Centímentros recorridos: '+ str(round(dist,1)))
+    print('Centímentros recorridos: '+ str(round(c,1)))
 
 
 
