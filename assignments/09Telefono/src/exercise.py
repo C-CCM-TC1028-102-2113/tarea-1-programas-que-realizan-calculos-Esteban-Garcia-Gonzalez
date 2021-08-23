31cb42774e25a314ff9b454134baeabd5db70f5a
Esteban-Garcia-Gonzalez <89358442+Esteban-Garcia-Gonzalez@users.noreply.github.com>
2021-08-22 20:30:26 -0500

    Update exercise.py

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,14 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
     msj= int(input('Dame el número de mensajes: '))
-    megas=round(float(input('Dame el número de megas: ')),1)
-    time= int(input('Dame el número de minutos: '))
+    mg=round(float(input('Dame el número de megas: ')),1)
+    t= int(input('Dame el número de minutos: '))
 
     msj=msj*0.8
-    megas=megas*0.8
-    time=time*0.8
+    mg=mg*0.8
+    t=t*0.8
 
-    total=msj+megas+time
+    total=msj+mg+t
 
     print('El costo mensual es: '+ str(total))
     pass
